951f5875019f23bcfd25e7052a2bed2545993fa6
albasalo <ana.luiza.basalo@gmail.com>
2011-12-10 11:18:44 -0200

    Fix de um bug nos checkboxes dos idiomas.

--- WebContent/js/checkbox.js
+++ WebContent/js/checkbox.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	$("#pt").attr('checked', true);
 				
 	$("#pt").click(function() {
+		$("#pt").attr('checked', true);
 		$("#en").attr('checked', false);
 	});
 	
 	$("#en").click(function() {
+		$("#en").attr('checked', true);
 		$("#pt").attr('checked', false);
 	});
 });
