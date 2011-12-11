553dba58a583d511e67f2201d6b593e72a51eec8
albasalo <ana.luiza.basalo@gmail.com>
2011-12-10 23:46:28 -0200

    Commit final (= código entregue para o TCC).

--- src/br/usp/cata/util/TextAnalyzerEN.java
+++ src/br/usp/cata/util/TextAnalyzerEN.java
@@ -1,20 +1,24 @@
 package br.usp.cata.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.ling.CoreLabel;
+import br.usp.cata.component.keywordExtraction.StopWords;
+import br.usp.cata.model.KeywordFrequencyPair;
+import br.usp.cata.model.Position;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
-
-import br.usp.cata.model.Position;
 
 
 public class TextAnalyzerEN extends TextAnalyzerLanguage {
@@ -45,18 +49,33 @@ public class TextAnalyzerEN extends TextAnalyzerLanguage {
 		int startToken, endToken, startLemma, endLemma;
 		Position startPosition, endPosition;
 		
+		HashMap<String, Integer> wordsFrequency = new HashMap<String, Integer>();
+		
 		for(String line : text) {
 			Annotation document = new Annotation(line);
 			pipeline.annotate(document);
 			
 			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+			
+			HashSet<String> stopWordsEN = StopWords.getStopWordsEN();
+			
 			for(CoreMap sentence: sentences) {
 				for(CoreLabel token: sentence.get(TokensAnnotation.class)) {
 					startPosition = new Position(lineNum, token.beginPosition());
 					endPosition = new Position(lineNum, token.endPosition());
 					
 					startToken = tokenizedText.size();				
-					String word = " " + token.get(TextAnnotation.class).toLowerCase();			
+					String word = " " + token.get(TextAnnotation.class).toLowerCase();	
+					
+					if(!stopWordsEN.contains(word) && !word.matches(".*\\d.*")) {
+						int frequency;
+						if(wordsFrequency.containsKey(word))
+							frequency = wordsFrequency.get(word) + 1;
+						else
+							frequency = 1;
+						wordsFrequency.put(word, frequency);
+					}			
+					
 					for(byte b : word.getBytes())
 						tokenizedText.add(b);			
 					endToken = tokenizedText.size() + blankBytesLength;
@@ -90,6 +109,19 @@ public class TextAnalyzerEN extends TextAnalyzerLanguage {
 		lemmatizedTextBytes = new byte[lemmatizedText.size()];
 		for(int i = 0; i < lemmatizedText.size(); i++)
 			lemmatizedTextBytes[i] = lemmatizedText.get(i);
+		
+		Iterator<String> wordsFrequencyIterator = wordsFrequency.keySet().iterator();
+		List<KeywordFrequencyPair> keywordsList = new ArrayList<KeywordFrequencyPair>();
+		while(wordsFrequencyIterator.hasNext()) {
+			String keyword = wordsFrequencyIterator.next();
+			keywordsList.add(new KeywordFrequencyPair(keyword, wordsFrequency.get(keyword)));
+		}
+		
+		Collections.sort(keywordsList);
+		
+		keywords = new ArrayList<String>();
+		for(int i = 0; i < 0.10*keywordsList.size(); i++)
+			keywords.add(keywordsList.get(i).getKeyword());
 	}
 	
 }
